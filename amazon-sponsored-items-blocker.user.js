6bce9c7f750016d0e6e2a1d5cb49ed2e974dff75
Stefan-Code <Stefan.github@gmail.com>
2016-01-18 20:16:45 +0100

    check if page content changed dynamically and remove new ad elements

--- amazon-sponsored-items-blocker.user.js
+++ amazon-sponsored-items-blocker.user.js
@@ -12,15 +12,25 @@
 // @run-at document-end
 // ==/UserScript==
 $ = jQuery.noConflict(true);
-$(document).ready(function() {
-    console.log("amazon-sponsored-items-blocker loaded");
-    var count = 0;
-$('.celwidget').each(function(i, obj) {
-    if ($(this).find(".s-sponsored-info-icon").length > 0){
-        //console.log("Object " + i + " contains an ad");
-        $(this).css('display', 'none');
-        count++;
+var pageContentchanged = false;
+$('body').bind("DOMSubtreeModified", function() {
+    pageContentchanged = true;
+});
+setInterval(removeSponsoredAds, 200);
+console.log("amazon-sponsored-items-blocker loaded");
+
+function removeSponsoredAds() {
+    if (pageContentchanged) {
+        var count = 0;
+        $('.celwidget').each(function(i, obj) {
+            if ($(this).find(".s-sponsored-info-icon").length > 0) {
+                //console.log("Object " + i + " contains an ad");
+                //$(this).css('background-color', 'red');
+                (this).remove();
+                count++;
+            }
+        });
+        console.log("amazon-sponsored-items-blocker: " + count + " ads removed!");
+        pageContentchanged = false;
+    }
 }
-});
-    console.log("amazon-sponsored-items-blocker: " + count + " ads removed!");
-});
