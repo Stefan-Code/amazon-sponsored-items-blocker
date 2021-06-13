9a2b7a1fbe6e74317ac4841778bd14a01a6d70f1
k.bigwheel (kazufumi nishida) <k.bigwheel+eng@gmail.com>
2021-06-14 01:12:39 +0900

    Support amazon.co.jp (#3)
    * Support amazon.co.jp
    
    * Update README.md

--- amazon-sponsored-items-blocker.user.js
+++ amazon-sponsored-items-blocker.user.js
@@ -7,6 +7,7 @@
 // @include      *://www.amazon.de/*
 // @include      *://www.amazon.com/*
 // @include      *://www.amazon.co.uk/*
+// @include      *://www.amazon.co.jp/*
 // @require      http://code.jquery.com/jquery-latest.js
 // @grant        none
 // @run-at document-end
