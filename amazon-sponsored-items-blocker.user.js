76da9274c8d4264f06dd7c33489f2d7933330c34
Yuya Ma'emichi <6386129+Wintus@users.noreply.github.com>
2021-09-07 00:37:37 +0900

    convert to let

--- amazon-sponsored-items-blocker.user.js
+++ amazon-sponsored-items-blocker.user.js
@@ -13,25 +13,25 @@
 // @run-at document-end
 // ==/UserScript==
 $ = jQuery.noConflict(true);
-var pageContentchanged = false;
-$('body').bind("DOMSubtreeModified", function() {
-    pageContentchanged = true;
+let pageContentchanged = false;
+$("body").bind("DOMSubtreeModified", function () {
+  pageContentchanged = true;
 });
 setInterval(removeSponsoredAds, 200);
 console.log("amazon-sponsored-items-blocker loaded");
 
 function removeSponsoredAds() {
-    if (pageContentchanged) {
-        var count = 0;
-        $('.celwidget').each(function(i, obj) {
-            if ($(this).find(".s-sponsored-label-info-icon").length > 0) {
-                //console.log("Object " + i + " contains an ad");
-                //$(this).css('background-color', 'red');
-                (this).remove();
-                count++;
-            }
-        });
-        console.log("amazon-sponsored-items-blocker: " + count + " ads removed!");
-        pageContentchanged = false;
-    }
+  if (pageContentchanged) {
+    let count = 0;
+    $(".celwidget").each(function (i, obj) {
+      if ($(this).find(".s-sponsored-label-info-icon").length > 0) {
+        //console.log("Object " + i + " contains an ad");
+        //$(this).css('background-color', 'red');
+        this.remove();
+        count++;
+      }
+    });
+    console.log("amazon-sponsored-items-blocker: " + count + " ads removed!");
+    pageContentchanged = false;
+  }
 }
